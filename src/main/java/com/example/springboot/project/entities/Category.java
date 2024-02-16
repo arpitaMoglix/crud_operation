5f3a4bcc111c94755706a0e2e586dcbd01e91bbe
arpitaMoglix <Arpita.kumari@moglix.com>
2024-02-16 13:08:32 +0530

    category entity and repository

--- src/main/java/com/example/springboot/project/entities/Category.java
+++ src/main/java/com/example/springboot/project/entities/Category.java
@@ -1,5 +1,25 @@
 package com.example.springboot.project.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(name = "category_name")
+    private String categoryName;
 
 }
+
